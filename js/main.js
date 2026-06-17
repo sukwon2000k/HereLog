cb02d62d7757d2dc361ae0f331fc62bdc00710bc
sukwon2000k <sukwon2000k@gmail.com>
2026-06-17 18:00:59 +0900

    HereLog2일차

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const createRoom = document.querySelector('.create-room');
 const createBox = document.querySelector('.create-box');
-
+const cancelBtn = document.querySelector('#cancelBtn')
+//새방만들기 누르면 창이 뜸
 createRoom.onclick = () => {
-    createBox.style.display = 'flex';
+    createBox.classList.add('show');
 };
+
+//방생성 누르면 방이 만들어짐
+
+//취소 누르면 입력창 사라짐
+cancelBtn.onclick = () => {
+    createBox.classList.remove('show');
+}
